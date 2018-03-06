73ca862c578efb2cfcf36363e0eb0ea26a264e63
Alex Clemens <alexclemens@WL-193-29.WPA.Claremont.Edu>
2018-03-06 12:54:21 -0800

    adding all current files - this includes the files that produce an error upon startup in the local environment

---
+++ clmtrackr-gh-pages/rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+
+export default {
+	entry: 'src/clm.js',
+	targets: [
+		{
+			format: 'umd',
+			moduleName: 'clm',
+			dest: 'build/clmtrackr.js'
+		},
+		{
+			format: 'es',
+			dest: 'build/clmtrackr.module.js'
+		}
+	],
+	plugins: [
+		resolve({
+			module: true,
+			main: true
+		}),
+		commonjs(),
+		json()
+	]
+	// ],
+	// globals: {
+	// 	'src/utils/debugging.js': 'src_utils_debugging_js'
+	// }
+};
